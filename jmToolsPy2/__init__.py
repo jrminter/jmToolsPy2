977362839afd3ebe4c03363db0cc6a8f888dd686
John Minter <john.r.minter@kodak.com>
2015-06-09 07:41:48 -0400

    trying two functions

---
+++ jmToolsPy2/__init__.py
@@ -0,0 +1,2 @@
+from .savitzky_golay import savitzky_golay
+from .peak_detect import peak_detect
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='jmToolsPy2',
+      version='0.0.90',
+      description='Utility functions for python 2.7',
+      url='http://github.com/jrminter/jmToolsPy2',
+      author='Many',
+      author_email='jrminter@gmail.com',
+      license='MIT',
+      packages=['jmToolsPy2'],
+      zip_safe=False)
