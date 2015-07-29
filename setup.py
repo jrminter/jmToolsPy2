495f56738878a76754503fa8f94c0ac23878d7f0
John Minter <john.r.minter@kodak.com>
2015-07-29 15:32:20 -0400

    added topHatFilter

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='jmToolsPy2',
-      version='0.0.92',
+      version='0.0.93',
       description='Utility functions for python 2.7',
       url='http://github.com/jrminter/jmToolsPy2',
       author='Many',
       author_email='jrminter@gmail.com',
-      license='MIT',
+      license='GPL-2',
       packages=['jmToolsPy2'],
       zip_safe=False)
