139aea117e0c7eba83b2d467f3338fe1ddc46c78
John Minter <jrminter@gmail.com>
2016-03-05 16:03:48 -0500

    added map tools and exception on hstools v. 0.0.959

--- jmToolsPy2/__init__.py
+++ jmToolsPy2/__init__.py
@@ -1,4 +1,10 @@
 from .savitzky_golay import savitzky_golay
 from .peak_detect import peak_detect
+try:
+ import hyperspy
+ from .hs_tools import *
+except ImportError, e:
+ pass # module doesn't exist, deal with it.
 # from .hs_tools import *
 from .plotting_tools import *
+from map_tools import *
